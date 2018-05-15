e6b6ae26c6f8f1bc60b9231e3a0e66e90fd8cf61
Aleksei Elin <leshaelin@gmail.com>
2018-05-16 01:46:34 +0300

    Test Start View

--- WaterPipes/Controllers/StartController.m
+++ WaterPipes/Controllers/StartController.m
@@ -8,6 +8,7 @@
 
 #import "StartController.h"
 #import "GameViewController.h"
+#import "ResultsViewController.h"
 
 @interface StartController ()
 
@@ -18,28 +19,69 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     
-    [[self view] setBackgroundColor:[UIColor redColor]];
-    [self setTitle:@"Start"];
+    [[self view] setBackgroundColor:[UIColor whiteColor]];
+    [self setTitle:@"Menu"];
     
-    UIButton *playButton = [[UIButton alloc] initWithFrame:CGRectMake(0, 0, 100, 40)];
+    UIButton *playButton = [[UIButton alloc] initWithFrame:CGRectMake(0, 0, 130, 40)];
     [playButton setCenter:CGPointMake([[self view] center].x, [[self view] center].y)];
-    [playButton setTitle:@"Play" forState:UIControlStateNormal];
-    [playButton setBackgroundColor:[UIColor greenColor]];
+    [playButton setTitle:@"START GAME" forState:UIControlStateNormal];
+    [playButton setBackgroundColor:[UIColor blueColor]];
     [playButton addTarget:self action:@selector(pushNextController:) forControlEvents:UIControlEventTouchUpInside];
     [[self view] addSubview:playButton];
     
+    UIButton *resultButton = [[UIButton alloc] initWithFrame:CGRectMake(0, 0, 130, 40)];
+    [resultButton setCenter:CGPointMake([[self view] center].x, 330)];
+    [resultButton setTitle:@"RESULTS" forState:UIControlStateNormal];
+    [resultButton setBackgroundColor:[UIColor blueColor]];
+    [resultButton addTarget:self action:@selector(clickResultButton:) forControlEvents:UIControlEventTouchUpInside];
+    [[self view] addSubview:resultButton];
+    
+    UILabel *teamLabel = [[UILabel alloc] initWithFrame:CGRectMake(0, 0, 150, 40)];
+    [teamLabel setCenter:CGPointMake([[self view] center].x,440)];
+    teamLabel.text = @"   WaterPipes Team";
+    [teamLabel setFont:[UIFont fontWithName:@"Helvetica" size:16]];
+    teamLabel.textColor = [UIColor lightGrayColor];
+    [[self view] addSubview:teamLabel];
+    
+    UILabel *titleLabel = [[UILabel alloc] initWithFrame:CGRectMake(0, 0, 220, 40)];
+    [titleLabel setCenter:CGPointMake([[self view] center].x,220)];
+    titleLabel.text = @" WaterPipes";
+    [titleLabel setFont:[UIFont fontWithName:@"Helvetica" size:38]];
+    titleLabel.textColor = [UIColor blueColor];
+    [[self view] addSubview:titleLabel];
+    
+    UILabel *teamNumbers = [[UILabel alloc] initWithFrame:CGRectMake(0, 0, 290, 40)];
+    [teamNumbers setCenter:CGPointMake([[self view] center].x,490)];
+    teamNumbers.lineBreakMode = NSLineBreakByWordWrapping;
+    teamNumbers.numberOfLines = 0;
+    teamNumbers.text = @"Elizaveta Shulskaya                  Victor Semenchuk Aliaksei Yelin                             Mark Velichko";
+    [teamNumbers setFont:[UIFont fontWithName:@"Helvetica" size:13]];
+    teamNumbers.textColor = [UIColor lightGrayColor];
+    [[self view] addSubview:teamNumbers];
+
+
+    
     [playButton release];
+    [resultButton release];
+    [teamLabel release];
+    [titleLabel release];
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+    
 }
 
 - (void)pushNextController:(UIButton *)sender {
     GameViewController *gameVC = [[GameViewController alloc] init];
     [[self navigationController] pushViewController:gameVC animated:true];
     [gameVC release];
+}
+
+- (void)clickResultButton:(UIButton *)sender {
+    ResultsViewController *resultsVC = [[ResultsViewController alloc] init];
+    [[self navigationController] pushViewController:resultsVC animated:true];
+    [resultsVC release];
 }
 
 /*
