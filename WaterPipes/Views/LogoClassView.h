fa611197d42e8545677068b05063539251043920
Aleksei Elin <leshaelin@gmail.com>
2018-05-20 20:16:44 +0300

    Add views

---
+++ WaterPipes/Views/LogoClassView.h
@@ -0,0 +1,13 @@
+//
+//  LogoClassView.h
+//  WaterPipes
+//
+//  Created by LESHA on 5/20/18.
+//  Copyright © 2018 Victor Semenchuk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LogoClassView : UIView
+
+@end
